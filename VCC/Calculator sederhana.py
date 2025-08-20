9ad59766a1ce90fa206ee06bffbfe302c6b99f72
Reymon <bimaarbs13@gmail.com>
2025-08-20 20:30:20 +0700

    Update and rename Aston.py to Calculator sederhana.py

--- VCC/Calculator sederhana.py
+++ VCC/Calculator sederhana.py
@@ -1,12 +1,18 @@
 nilai_a = int(input("MASUKAN NILAI A: "))
 nilai_b = int(input("MASUKAN NILAI B: "))
-operasi = input("MASUKAN OPERASI (+, -, *,): ")
-hasil = nilai_a + nilai_b 
+operasi = input("MASUKAN OPERASI (+, -, *, /): ")
+
+# Program Kalkulator Sederhana
 if operasi == "+":
     print(f"Hasil dari {nilai_a} + {nilai_b} = {nilai_a + nilai_b}")
 elif operasi == "-":
     print(f"Hasil {nilai_a} - {nilai_b} = {nilai_a - nilai_b}")
 elif operasi == "*":
     print(f"Hasil {nilai_a} * {nilai_b} = {nilai_a * nilai_b}")
+elif operasi == "/":
+    if nilai_b != 0:
+        print(f"Hasil {nilai_a} / {nilai_b} = {nilai_a / nilai_b}")
+    else:
+        print("Pembagian dengan nol tidak diperbolehkan.")
 else :
     print("Operasi tidak dikenali.")
